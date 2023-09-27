acc5e7b2f31a4b825edfb4c1e117e621fc7e865c
nguyendy630 <nguyendylan2468@gmail.com>
2023-09-26 22:37:02 -0400

    Dylan's Website

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+ return(
+  <>
+  {/* Navbar */}
+  <header>
+    <nav>
+      <ul className="nav-links">
+        <li class="nav-items"><a href='#home'>Home</a></li>
+        <li class="nav-items"><a href='#about'>About</a></li>
+        <li class="nav-items"><a href='#projects'>Projects</a></li>
+        <li class="nav-items"><a href='#contact'>Contact</a></li>
+        
+      </ul>
+    </nav>
+  </header>
+  {/* End of Navbar */}
+  
+  
+  </>
+
+ )
+}
+
+export default App
