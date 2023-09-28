75306d20a2170c00f046d7b4b15e039f3dd86731
nguyendy630 <nguyendylan2468@gmail.com>
2023-09-27 23:37:38 -0400

    Icon Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import './App.css'
+import { ImHome3 } from "react-icons/im";
+import { IoIosContact } from "react-icons/io";
+import { GiLightBulb } from "react-icons/gi";
+import {AiOutlineMail} from "react-icons/ai"
 
 function App() {
  return(
@@ -7,23 +11,29 @@ function App() {
   <header>
     <nav>
       <ul className="nav-links">
-        <li class="nav-items"><a href='#home'>Home</a></li>
-        <li class="nav-items"><a href='#about'>About</a></li>
-        <li class="nav-items"><a href='#projects'>Projects</a></li>
-        <li class="nav-items"><a href='#contact'>Contact</a></li>
+        <li class="nav-items"><a href='#home'><ImHome3></ImHome3></a></li>
+        <li class="nav-items"><a href='#about'><IoIosContact></IoIosContact></a></li>
+        <li class="nav-items"><a href='#projects'><GiLightBulb></GiLightBulb></a></li>
+        <li class="nav-items"><a href='#contact'><AiOutlineMail></AiOutlineMail></a></li>
         
       </ul>
     </nav>
   </header>
   {/* End of Navbar */}
+
+  {/* Hero Section */}
   <section>
     <div className="container">
       <h1>Dylan Nguyen</h1>
       <h2>Junior Software Developer</h2>
       <p>Dedicated Knowledge Connoisseur with a Passion for Lifelong Learning in the world software development.</p>
-      <button className="resume-button">Résumé</button>
+      <button className="resume-button">Résume</button>
     </div>
   </section>
+  {/* end of Hero Section */}
+
+  {/* About Section */}
+  
   
   </>
 
