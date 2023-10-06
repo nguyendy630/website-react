36c548ec99154fb967bd46359f3710913ad5f5b2
Dylan Nguyen <112450882+nguyendy630@users.noreply.github.com>
2023-10-06 13:34:52 -0400

    Updated Buttion

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import { ImHome3 } from "react-icons/im";
 import { IoIosContact } from "react-icons/io";
 import { GiLightBulb } from "react-icons/gi";
 import {AiOutlineMail} from "react-icons/ai"
+import { link } from "react-router-dom";
 
 function App() {
+  function buttonLink({to, children}) {
+    return <Link to={to}><button>{children}</button></Link>
+  }
  return(
   <>
   {/* Navbar */}
@@ -14,8 +18,7 @@ function App() {
         <li class="nav-items"><a href='#home'><ImHome3></ImHome3></a></li>
         <li class="nav-items"><a href='#about'><IoIosContact></IoIosContact></a></li>
         <li class="nav-items"><a href='#projects'><GiLightBulb></GiLightBulb></a></li>
-        <li class="nav-items"><a href='#contact'><AiOutlineMail></AiOutlineMail></a></li>
-        
+        <li class="nav-items"><a href='#contact'><AiOutlineMail></AiOutlineMail></a></li>  
       </ul>
     </nav>
   </header>
@@ -25,19 +28,17 @@ function App() {
   <section>
     <div className="container">
       <h1>Dylan Nguyen</h1>
-      <h2>Junior Software Developer</h2>
+      <h2>Frontend Web Developer
+      </h2>
       <p>Dedicated Knowledge Connoisseur with a Passion for Lifelong Learning in the world software development.</p>
-      <button className="resume-button">Résume</button>
+      <button className="resume-button" onClick={buttonLink}>Résume</button>
     </div>
   </section>
   {/* end of Hero Section */}
 
   {/* About Section */}
   
-  
   </>
-
  )
 }
-
 export default App
